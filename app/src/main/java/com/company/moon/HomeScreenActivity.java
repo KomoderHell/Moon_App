231a784169b437f4f203fdb383d4eb486ffc98c2
Apoorva Raj Bhadani <apoorva222g@gmail.com>
2019-12-07 21:57:30 +0530

    HomeScreen Basic layout created p2

--- app/src/main/java/com/company/moon/HomeScreenActivity.java
+++ app/src/main/java/com/company/moon/HomeScreenActivity.java
@@ -1,14 +1,82 @@
 package com.company.moon;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeScreenActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.Open,R.string.Close);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final NavigationView navigationView = (NavigationView)findViewById(R.id.navigation);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if(id==R.id.nav_register_as_distributer){
+                    Toast.makeText(HomeScreenActivity.this,"Reg as dist",Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+
+        BottomNavigationView bottomNavView = findViewById(R.id.bottom_navigation);
+        bottomNavView.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FragmentHome()).commit();
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    switch (menuItem.getItemId()){
+                        case R.id.bnv_home:
+                            selectedFragment = new FragmentHome();
+                            break;
+                        case R.id.bnv_notification:
+                            selectedFragment = new FragmentNotification();
+                            break;
+                        case R.id.bnv_cart:
+                            selectedFragment = new FragmentCart();
+                            break;
+                        case R.id.bnv_Profile:
+                            selectedFragment = new FragmentProfile();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                    return true;
+                }
+            };
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return actionBarDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 }
