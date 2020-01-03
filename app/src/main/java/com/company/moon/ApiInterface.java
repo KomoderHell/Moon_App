2620ac849c4783f96bd93c2284ba391cf4c925d0
razor7386 <48329731+razor7386@users.noreply.github.com>
2020-01-04 00:27:50 +0530

    Update ApiInterface.java

--- app/src/main/java/com/company/moon/ApiInterface.java
+++ app/src/main/java/com/company/moon/ApiInterface.java
@@ -1,5 +1,7 @@
 package com.company.moon;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
@@ -8,10 +10,25 @@ import retrofit2.http.POST;
 public interface ApiInterface {
     @FormUrlEncoded
     @POST("signup.php")
-    Call<Integer> signUpUser(@Field("email") String email,
+    Call<List<LogInInfo>> signUpUser(@Field("email") String email,
                              @Field("password") String password,
                              @Field("confirm") String confirm,
                              @Field("phone") String phone,
                              @Field("type") String type)
+        ;
+
+    @FormUrlEncoded
+    @POST("signup.php")
+    Call<Integer> signUpUser2(@Field("email") String email,
+                              @Field("password") String password,
+                              @Field("confirm") String confirm,
+                              @Field("phone") String phone,
+                              @Field("type") String type)
+            ;
+
+    @FormUrlEncoded
+    @POST("login.php")
+    Call<List<LogInInfo>> logInUser(@Field("email") String email,
+                         @Field("password") String password)
         ;
 }
