5f49f1e59a9bff70006b2ddb946c150ee75526c9
razor7386 <48329731+razor7386@users.noreply.github.com>
2020-01-04 00:27:20 +0530

    Update ApiRequest.java

--- app/src/main/java/com/company/moon/ApiRequest.java
+++ app/src/main/java/com/company/moon/ApiRequest.java
@@ -1,5 +1,7 @@
 package com.company.moon;
 
+import java.util.List;
+
 import retrofit2.Callback;
 
 public class ApiRequest {
@@ -11,8 +13,17 @@ public class ApiRequest {
         }
         return instance;
     }
-    
-    public void signUpUser(String email, String password, String confirm, String phone, String type, Callback<Integer> callback) {
+
+    // Other Functions
+    public void signUpUser(String email, String password, String confirm, String phone, String type, Callback<List<LogInInfo>> callback) {
         ApiClient.getClient().create(ApiInterface.class).signUpUser(email, password, confirm, phone, type).enqueue(callback);
+    }
+
+    public void signUpUser2(String email, String password, String confirm, String phone, String type, Callback<Integer> callback) {
+        ApiClient.getClient().create(ApiInterface.class).signUpUser2(email, password, confirm, phone, type).enqueue(callback);
+    }
+
+    public void logInUser(String email, String password, Callback<List<LogInInfo>> callback) {
+        ApiClient.getClient().create(ApiInterface.class).logInUser(email, password).enqueue(callback);
     }
 }
