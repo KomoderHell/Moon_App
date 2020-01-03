f1382ae7f4747d5e610593541c2e8904b4b18025
razor7386 <48329731+razor7386@users.noreply.github.com>
2020-01-03 19:29:53 +0530

    Update MainActivity.java

--- app/src/main/java/com/company/moon/MainActivity.java
+++ app/src/main/java/com/company/moon/MainActivity.java
@@ -3,23 +3,44 @@ package com.company.moon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SQLiteDatabase userInfoDatabase;
+
     android.os.Handler Handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Creating the database if not exists already
+        userInfoDatabase = this.openOrCreateDatabase("UserInfoDatabase", MODE_PRIVATE, null);
+        // user_id --> Id of the user
+        // type --> admin, manufacturer, retailer
+        userInfoDatabase.execSQL("CREATE TABLE IF NOT EXISTS UserInfo(user_id varchar2(20), type varchar2(1))");
+
         Handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(MainActivity.this,SignInActivity.class);
+                Cursor cursor = userInfoDatabase.rawQuery("SELECT * FROM UserInfo", null);
+                Intent intent = null;
+                if (cursor.getCount() > 0) {
+                    // Go to HomeScreenActivity (Remember me was clicked before)
+                    cursor.moveToFirst();
+                    intent = new Intent(MainActivity.this, HomeScreenActivity.class);
+                    intent.putExtra("Type", cursor.getString(cursor.getColumnIndex("type")));
+                } else {
+                    intent = new Intent(MainActivity.this, SignInActivity.class);
+                }
                 startActivity(intent);
                 finish();
             }
-        },2500);
+        }, 2500);
     }
 }
