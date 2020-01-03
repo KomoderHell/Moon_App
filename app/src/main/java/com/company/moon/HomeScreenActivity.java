6cd4efd66e29a49dfd57959c4ca5abac2d8febde
razor7386 <48329731+razor7386@users.noreply.github.com>
2020-01-03 19:07:02 +0530

    Update HomeScreenActivity.java

--- app/src/main/java/com/company/moon/HomeScreenActivity.java
+++ app/src/main/java/com/company/moon/HomeScreenActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
@@ -43,16 +44,40 @@ public class HomeScreenActivity extends AppCompatActivity {
 
         final NavigationView navigationView = (NavigationView)findViewById(R.id.navigation);
 
+        navigationView.inflateMenu(R.menu.navigation_menu);
+
+        /*  This will be changed later
+
+        if (getIntent().getStringExtra("Type") != null) {
+            String type = getIntent().getStringExtra("Type");
+            if (type.equals("1")) {
+                // Admin
+            } else if (type.equals("2")) {
+                // Manufacturer
+            } else if (type.equals("3")) {
+                // Retailer
+                navigationView.inflateMenu(R.menu.navigation_menu);
+            }
+        }
+
+         */
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 int id = menuItem.getItemId();
-                if(id==R.id.nav_register_as_distributer){
-                    startActivity(new Intent(HomeScreenActivity.this,ActivityDistributer.class));
+                switch (id) {
+                    case R.id.nav_update_profile:
+                        // Open activity to update profile
+                        startActivity(new Intent(HomeScreenActivity.this, RetailerRegistrationActivity.class));
+                        break;
+                    case R.id.nav_privacy_policy:
+                        break;
+                    case R.id.nav_about_us:
+                        break;
+                    case R.id.nav_settings:
+                        break;
                 }
-                else if (id==R.id.nav_register_as_reseller){
-                    startActivity(new Intent(HomeScreenActivity.this,RetailerRegistrationActivity.class));
-                }
+                closeDrawer();
                 return true;
             }
         });
@@ -91,5 +116,20 @@ public class HomeScreenActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return actionBarDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    private void closeDrawer() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
