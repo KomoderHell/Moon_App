0725409dfdbfff7528badc8bda85f2e15f04c84f
razor7386 <48329731+razor7386@users.noreply.github.com>
2020-01-03 14:32:02 +0530

    Update SignInActivity.java

--- app/src/main/java/com/company/moon/SignInActivity.java
+++ app/src/main/java/com/company/moon/SignInActivity.java
@@ -17,12 +17,14 @@ import android.widget.Toast;
 public class SignInActivity extends AppCompatActivity {
 
     ConstraintLayout toolbar;
-    EditText editTextMobileEmail , editTextPassword;
+    EditText editTextMobileEmail, editTextPassword;
     CheckBox checkBoxRemember;
-    Button buttonLogin , buttonRegister;
+    Button buttonLogin, buttonRegister;
 
-    String phoneEmail,password;
+    String phoneEmail, password;
     Boolean boolRemember;
+
+    private ApiRequest apiRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +42,7 @@ public class SignInActivity extends AppCompatActivity {
         buttonLogin = findViewById(R.id.buttonNextDistributerAdd2);
         buttonRegister = findViewById(R.id.buttonRegister);
 
+        apiRequest = ApiRequest.getInstance();
 
         checkBoxRemember.setChecked(true);
 
@@ -53,32 +56,60 @@ public class SignInActivity extends AppCompatActivity {
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SignInActivity.this,SignUpActivity.class));
+                startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+                finish();
             }
         });
     }
 
-    public void loginUser(){
+    public void loginUser() {
         phoneEmail = editTextMobileEmail.getText().toString().trim();
         password = editTextPassword.getText().toString().trim();
         boolRemember = checkBoxRemember.isChecked();
 
-        if (phoneEmail.isEmpty()){
+        if (phoneEmail.isEmpty()) {
             editTextMobileEmail.setError("This field cannot be empty");
-        }
-        else if (!Patterns.EMAIL_ADDRESS.matcher(phoneEmail).matches()&&!Patterns.PHONE.matcher(phoneEmail).matches()){
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(phoneEmail).matches() && !Patterns.PHONE.matcher(phoneEmail).matches()) {
             editTextMobileEmail.setError("Enter a valid phone number or email");
-        }
-        else if (password.isEmpty()){
+        } else if (password.isEmpty()) {
             editTextPassword.setError("This field cannot be empty");
-        }
-        else if (password.length()<8){
+        } else if (password.length() < 8) {
             editTextPassword.setError("Enter a valid Password");
-        }
-        else{
-            Toast.makeText(this, "Login\n" + boolRemember , Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(SignInActivity.this,HomeScreenActivity.class));
-            finish();
+        } else {
+            // Show progress bar while we sign in the user
+            ProgressDialog dialog = new ProgressDialog(SignInActivity.this);
+            dialog.setMessage("Signing In...");
+            dialog.show();
+
+            apiRequest.logInUser(phoneEmail, password, new Callback<List<LogInInfo>>() {
+                @Override
+                public void onResponse(Call<List<LogInInfo>> call, Response<List<LogInInfo>> response) {
+                    if (!response.isSuccessful()) {
+                        Log.d("retro:", "onResponse: response failed");
+                        Toast.makeText(SignInActivity.this, "Login failed!!!", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                        return;
+                    }
+                    Log.d("retro:", "onResponse: " + "response OK");
+                    dialog.dismiss();
+
+                    Intent intent = new Intent(SignInActivity.this, HomeScreenActivity.class);
+
+                    List<LogInInfo> user = response.body();
+                    // Only 1 object is present
+                    String type = user.get(0).getType();
+                    intent.putExtra("Type", type);
+                    startActivity(intent);
+                    finish();
+                }
+
+                @Override
+                public void onFailure(Call<List<LogInInfo>> call, Throwable t) {
+                    Log.d("retro:", "onFailure: " + t.getLocalizedMessage());
+                    Toast.makeText(SignInActivity.this, "Login failed!!!", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+            });
         }
     }
 
