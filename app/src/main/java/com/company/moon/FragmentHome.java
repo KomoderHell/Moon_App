72ac001f184db65ded8288546c784de52451e8af
razor7386 <48329731+razor7386@users.noreply.github.com>
2020-01-06 00:42:18 +0530

    Update FragmentHome.java

--- app/src/main/java/com/company/moon/FragmentHome.java
+++ app/src/main/java/com/company/moon/FragmentHome.java
@@ -1,6 +1,7 @@
 package com.company.moon;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,11 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FragmentHome extends Fragment {
 
@@ -25,6 +31,11 @@ public class FragmentHome extends Fragment {
 
     private ArrayAdapter<String> companyAdapter;
     private ArrayAdapter<String> productAdapter;
+
+    private ArrayList<String> companies;
+    private ArrayList<String> products;
+
+    private ApiRequest apiRequest;
 
     @Nullable
     @Override
@@ -38,16 +49,21 @@ public class FragmentHome extends Fragment {
         sort = view.findViewById(R.id.sort_home_fragment);
         recyclerView = view.findViewById(R.id.recycler_view_home_fragment);
 
+        apiRequest = ApiRequest.getInstance();
+
         // Spinner Adapters
-        ArrayList<String> companies = new ArrayList<>();
+        companies = new ArrayList<>();
         companies.add("Companies");
         companyAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item, companies);
         companiesSpinner.setAdapter(companyAdapter);
 
-        ArrayList<String> products = new ArrayList<>();
+        products = new ArrayList<>();
         products.add("Products");
         productAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item, products);
         productsSpinner.setAdapter(productAdapter);
+
+        // Load names from API
+        getProductAndCompanyNames();
 
         // Clicks
         filter.setOnClickListener(new View.OnClickListener() {
@@ -65,5 +81,32 @@ public class FragmentHome extends Fragment {
         });
 
         return view;
+    }
+
+    private void getProductAndCompanyNames() {
+        apiRequest.getProductCompanyNames(new Callback<List<ModelNames>>() {
+            @Override
+            public void onResponse(Call<List<ModelNames>> call, Response<List<ModelNames>> response) {
+                if (!response.isSuccessful()) {
+                    Log.d("retro:", "onResponse: response failed" + response.code());
+                }
+                //Log.d("retro:", "onResponse: OK");
+                List<ModelNames> names = response.body();
+                for (ModelNames name : names) {
+                    String productName = name.getProduct_name();
+                    products.add(productName);
+                    productAdapter.notifyDataSetChanged();
+
+                    String companyName = name.getName();
+                    companies.add(companyName);
+                    companyAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<ModelNames>> call, Throwable t) {
+                Log.d("retro:", "onFailure: " + t.getLocalizedMessage());
+            }
+        });
     }
 }
