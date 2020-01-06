62da581efca95906c313f0d5d4b2f1095bbfb885
razor7386 <48329731+razor7386@users.noreply.github.com>
2020-01-07 01:54:11 +0530

    Update FragmentHome.java

--- app/src/main/java/com/company/moon/FragmentHome.java
+++ app/src/main/java/com/company/moon/FragmentHome.java
@@ -1,10 +1,15 @@
 package com.company.moon;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -12,9 +17,11 @@ import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import retrofit2.Call;
@@ -31,9 +38,16 @@ public class FragmentHome extends Fragment {
 
     private ArrayAdapter<String> companyAdapter;
     private ArrayAdapter<String> productAdapter;
+    private AdapterProductsHomeFragment adapterProductsHomeFragment;
 
     private ArrayList<String> companies;
     private ArrayList<String> products;
+    private ArrayList<ModelProductDetail> displayProducts;
+
+    // API parameters
+    private String productType, companiesType;
+
+    private SQLiteDatabase productCompanyDatabase;
 
     private ApiRequest apiRequest;
 
@@ -51,19 +65,37 @@ public class FragmentHome extends Fragment {
 
         apiRequest = ApiRequest.getInstance();
 
+        productCompanyDatabase = getActivity().openOrCreateDatabase("ProductCompanyDatabase", Context.MODE_PRIVATE, null);
+        productCompanyDatabase.execSQL("DELETE FROM Companies");
+        productCompanyDatabase.execSQL("DELETE FROM Products");
+
+        displayProducts = new ArrayList<>();
+
         // Spinner Adapters
         companies = new ArrayList<>();
         companies.add("Companies");
         companyAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item, companies);
         companiesSpinner.setAdapter(companyAdapter);
+        productCompanyDatabase.execSQL("INSERT INTO Companies(Name, ID) VALUES('Companies', '0')");
 
         products = new ArrayList<>();
         products.add("Products");
         productAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item, products);
         productsSpinner.setAdapter(productAdapter);
+        productCompanyDatabase.execSQL("INSERT INTO Products(Name, ID) VALUES('Products', '0')");
 
-        // Load names from API
-        getProductAndCompanyNames();
+        // Recycler View Adapter
+        adapterProductsHomeFragment = new AdapterProductsHomeFragment(getActivity(), displayProducts, getActivity().getSupportFragmentManager());
+        recyclerView.setAdapter(adapterProductsHomeFragment);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // Default values of variables
+        productType = "0";
+        companiesType = "0";
+
+        // Load company names and product names through APIs
+        loadCompanyNames();
+        loadProductNames();
 
         // Clicks
         filter.setOnClickListener(new View.OnClickListener() {
@@ -80,32 +112,132 @@ public class FragmentHome extends Fragment {
             }
         });
 
-        return view;
-    }
-
-    private void getProductAndCompanyNames() {
-        apiRequest.getProductCompanyNames(new Callback<List<ModelNames>>() {
+        productsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onResponse(Call<List<ModelNames>> call, Response<List<ModelNames>> response) {
-                if (!response.isSuccessful()) {
-                    Log.d("retro:", "onResponse: response failed" + response.code());
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Cursor cursor = productCompanyDatabase.rawQuery("SELECT * FROM Products WHERE Name=?",
+                        new String[] {products.get(position)});
+                if (cursor.getCount() > 0) {
+                    cursor.moveToFirst();
+                    productType = cursor.getString(cursor.getColumnIndex("ID"));
+                    Log.d("sqlite:", "onItemSelected: " + productType);
                 }
-                //Log.d("retro:", "onResponse: OK");
-                List<ModelNames> names = response.body();
-                for (ModelNames name : names) {
-                    String productName = name.getProduct_name();
-                    products.add(productName);
-                    productAdapter.notifyDataSetChanged();
-
-                    String companyName = name.getName();
-                    companies.add(companyName);
-                    companyAdapter.notifyDataSetChanged();
-                }
+                displayProducts.clear();
+                loadProductDetails();
             }
 
             @Override
-            public void onFailure(Call<List<ModelNames>> call, Throwable t) {
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        companiesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Cursor cursor = productCompanyDatabase.rawQuery("SELECT * FROM Companies WHERE Name=?",
+                        new String[] {companies.get(position)});
+                if (cursor.getCount() > 0) {
+                    cursor.moveToFirst();
+                    companiesType = cursor.getString(cursor.getColumnIndex("ID"));
+                    Log.d("sqlite:", "onItemSelected: " + companiesType);
+                }
+                displayProducts.clear();
+                loadProductDetails();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        return view;
+    }
+
+    private void loadCompanyNames() {
+        apiRequest.getCompanies(new Callback<List<ModelCompany>>() {
+            @Override
+            public void onResponse(Call<List<ModelCompany>> call, Response<List<ModelCompany>> response) {
+                if (!response.isSuccessful()) {
+                    Log.d("retro:", "onResponse: response failed" + response.code());
+                }
+                List<ModelCompany> names = response.body();
+                for (ModelCompany companyName : names) {
+                    String nameStr = companyName.getName();
+                    String usIDStr = companyName.getUs_id();
+                    companies.add(nameStr);
+                    companyAdapter.notifyDataSetChanged();
+
+                    // Add this data to SQLite Database
+                    String sql = "INSERT INTO Companies(Name, ID) VALUES(?, ?)";
+                    SQLiteStatement statement = productCompanyDatabase.compileStatement(sql);
+                    statement.bindString(1, nameStr);
+                    statement.bindString(2, usIDStr);
+                    statement.execute();
+                }
+                Collections.sort(companies.subList(1, companies.size()));
+                companyAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<ModelCompany>> call, Throwable t) {
                 Log.d("retro:", "onFailure: " + t.getLocalizedMessage());
+            }
+        });
+    }
+
+    private void loadProductNames() {
+        apiRequest.getProducts(new Callback<List<ModelProduct>>() {
+            @Override
+            public void onResponse(Call<List<ModelProduct>> call, Response<List<ModelProduct>> response) {
+                if (!response.isSuccessful()) {
+                    Log.d("retro:", "onResponse: response failed" + response.code());
+                }
+                List<ModelProduct> models = response.body();
+                for (ModelProduct product : models) {
+                    String type = product.getProduct_type();
+                    String typeIDStr = product.getType_id();
+                    products.add(type);
+                    productAdapter.notifyDataSetChanged();
+
+                    // Add this data to SQLite Database
+                    String sql = "INSERT INTO Products(Name, ID) VALUES(?, ?)";
+                    SQLiteStatement statement = productCompanyDatabase.compileStatement(sql);
+                    statement.bindString(1, type);
+                    statement.bindString(2, typeIDStr);
+                    statement.execute();
+                }
+                Collections.sort(products.subList(1, products.size()));
+                productAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<ModelProduct>> call, Throwable t) {
+                Log.d("retro:", "onFailure: " + t.getLocalizedMessage());
+            }
+        });
+    }
+
+    private void loadProductDetails() {
+        // This data is shown on the recycler view
+        apiRequest.getProductDetails(productType, companiesType, new Callback<List<ModelProductDetail>>() {
+            @Override
+            public void onResponse(Call<List<ModelProductDetail>> call, Response<List<ModelProductDetail>> response) {
+                if (!response.isSuccessful()) {
+                    Log.d("retro:", "onResponse: response failed" + response.code());
+                }
+                List<ModelProductDetail> things = response.body();
+                for (ModelProductDetail thing : things) {
+                    displayProducts.add(thing);
+                    adapterProductsHomeFragment.notifyDataSetChanged();
+                }
+                //Log.d("retro:", "onResponse: " + things.get(0).getProduct_name());
+            }
+
+            @Override
+            public void onFailure(Call<List<ModelProductDetail>> call, Throwable t) {
+                Log.d("retro: ", "onFailure: " + t.getLocalizedMessage());
             }
         });
     }
